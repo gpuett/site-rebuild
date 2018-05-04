f378cc23d3389b299661c6926b1341bc0f749ac1
Garnett Puett <gpuett@gmail.com>
2018-05-04 09:44:43 -0700

    initial editors' picks section component implementation

--- src/app/editor-picks/editor-picks.component.ts
+++ src/app/editor-picks/editor-picks.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Media } from '../models/media.model';
 
 @Component({
   selector: 'app-editor-picks',
@@ -6,8 +7,9 @@ import { Component, Input, Output } from '@angular/core';
   styleUrls: ['./editor-picks.component.css']
 })
 export class EditorPicksComponent {
+  @Input() childMediaList: Media[];
+  @Output() clickSender = new EventEmitter();
 
-  constructor() { }
 
 
 }
