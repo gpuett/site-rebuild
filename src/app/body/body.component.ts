01daf9a14d6a7e1460d00ca3e24adb8676d9c10d
Garnett Puett <gpuett@gmail.com>
2018-05-04 15:17:39 -0700

    Add tables for shows and albums

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,6 +3,8 @@ import { Media } from '../models/media.model';
 import { Thumb } from '../models/thumb.model';
 import { Movie } from '../models/movie.model';
 import { Game } from '../models/game.model';
+import { Show } from '../models/tv.model';
+import { Album } from '../models/music.model';
 
 @Component({
   selector: 'app-body',
@@ -45,4 +47,29 @@ export class BodyComponent {
     new Game("Dead In Vinland","PC",79)
   ];
 
+  shows: Show[] = [
+    new Show("Sweetbitter: Season 1","Starz",48),
+    new Show("Bobby Kennedy for President","Netflix",72),
+    new Show("Genius (2017): Season 2","National Geographic",52),
+    new Show("Elvis Presley: The Searcher","HBO",84),
+    new Show("Rellik: Season 1","Cinemax",51),
+    new Show("Lost in Space (2018): Season 1","Netflix",76),
+    new Show("Andre the Giant: Season 1","HBO",76),
+    new Show("Killing Eve: Season 1","BBC America",83),
+    new Show("Howards End","Starz",86),
+    new Show("Paterno","HBO",68),
+  ];
+
+  albums: Album[] = [
+    new Album("World's Strongest Man","Gaz Coombes",83),
+    new Album("Knock Knock","DJ Koze",83),
+    new Album("The Horizon Laughed","Damien Jurado",72),
+    new Album("Dove","Belly",71),
+    new Album("Good Thing","Leon Bridges",74),
+    new Album("Vive La Void","Vive La Void",71),
+    new Album("Music for Installtions [Box Set]","Brian Eno",79),
+    new Album("Panic Blooms","Black Moth Super Rainbow",69),
+    new Album("Caer","Twin Shadow",67),
+    new Album("Last Will Standing","Willie Nelson",81),
+  ];
 }
