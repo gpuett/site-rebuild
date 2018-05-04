cce4655e084a403ca89c54bfe122748c3f180f5c
Garnett Puett <gpuett@gmail.com>
2018-05-04 16:02:40 -0700

    overlay metascore image and rating in editors picks component.

--- src/app/editor-picks/editor-picks.component.ts
+++ src/app/editor-picks/editor-picks.component.ts
@@ -12,10 +12,10 @@ export class EditorPicksComponent {
   @Output() clickSender = new EventEmitter();
 
   editorMedia: Media[] = [
-    new Media("METASCORE PREDICTIONS","Here are the predictions from over 4,500 Metacritic users for the final Metascores for summer's 30 biggest films.",'./../assets/metascore_predictions.png'),
-    new Media("TULLY","Jason Reitman re-teams with his Juno screenwriter Diablo Cody for a new film that ranks among the director's best.",'./../assets/tully.jpg'),
-    new Media("10 FILMS TO SEE IN MAY","Preview this month's top theatrical releases, including Deadpool 2 and Solo: A Star Wars Story.",'./../assets/preview.png'),
-    new Media("DONKEY KONG COUNTRY","The 2014 Wii U platformer Tropical Freeze comes to the Switch this week in a new port that critics seem to like.",'./../assets/dk.jpg'),
+    new Media("METASCORE PREDICTIONS","Here are the predictions from over 4,500 Metacritic users for the final Metascores for summer's 30 biggest films.",'./../assets/metascore_predictions.png', './../assets/favorable.png', "??"),
+    new Media("TULLY","Jason Reitman re-teams with his Juno screenwriter Diablo Cody for a new film that ranks among the director's best.",'./../assets/tully.jpg','./../assets/favorable.png',"77"),
+    new Media("10 FILMS TO SEE IN MAY","Preview this month's top theatrical releases, including Deadpool 2 and Solo: A Star Wars Story.",'./../assets/preview.png','http://www.metacritic.com/images/buttons/hpb/Article-00.png', "" ),
+    new Media("DONKEY KONG COUNTRY","The 2014 Wii U platformer Tropical Freeze comes to the Switch this week in a new port that critics seem to like.",'./../assets/dk.jpg','./../assets/favorable.png',"87" ),
   ];
 
 }
--- src/app/models/media.model.ts
+++ src/app/models/media.model.ts
@@ -1,3 +1,3 @@
 export class Media {
-  constructor(public title: string, public description: string, public imgString: string) { }
+  constructor(public title: string, public description: string, public imgString: string, public subImg: string, public metascore: string) { }
 }
