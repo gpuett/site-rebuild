90794490414dbd737d074b80d1c6a10bb49c591c
Garnett Puett <gpuett@gmail.com>
2018-05-11 08:18:48 -0700

    Add authentication service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Pick } from './models/pick.model';
 import { Thumb } from './models/thumb.model';
 import { EditorPicksComponent } from './editor-picks/editor-picks.component';
 import { BodyComponent } from './body/body.component';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthenticationService]
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }  from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { masterFirebaseConfig } from './api-keys';
 
 import { AppComponent } from './app.component';
